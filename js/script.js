17ed32f490a7d2f37fce497a43e8f06242f645a5
Kingkongos5 <113205091+Kingkongos5@users.noreply.github.com>
2024-03-16 01:21:21 +0200

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,21 +1,37 @@
 
 // Header ==============================================
 
+const header = document.querySelector('.header')
 const burger = document.querySelector('.header__burger');
 const nav = document.querySelector('.nav__items');
 
-burger.addEventListener("click", openBurger);
+addEventListener("click", openBurger);
 
-function openBurger(e){
-   if (e.target.closest('.haeder__burger')){
+function openBurger(e) {
+
+   if (e.target.closest('.header__burger')) {
       burger.classList.toggle('active');
       nav.classList.toggle('active');
    }
-   if (!e.target.closest('.haeder__burger')){
-      burger.classList.toggle('active');
-      nav.classList.toggle('active');
+   if (!e.target.closest('.header__burger')) {
+      burger.classList.remove('active');
+      nav.classList.remove('active');
    }
 }
 
-// =====================================================
+// Header - animation ==================================
 
+const observ = new IntersectionObserver((entries) => {
+   entries.forEach((entry) => {
+      console.log(123);
+      entry.isIntersecting ? header.classList.add("active") : header.classList.remove('active');
+   });
+},
+   {
+      threshold: [0.6]
+   }
+);
+
+
+
+//document.querySelectorAll('.page2').forEach((page) => {observ.observe(page)})
